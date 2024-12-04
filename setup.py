c55fd9605a22ee6771343915422d80317abb5568
Jonas Dech <j.dech@gmx.net>
2024-12-04 13:57:06 +0100

    updated project structure

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
-#!/usr/bin/env python
+from setuptools import find_packages, setup
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+package_name = 'kdl_ik_service'
 
-# fetch values from package.xml
-setup_args = generate_distutils_setup(
-    packages=['kdl_ik_service'],
-    package_dir={'': 'src'},
+setup(
+    name=package_name,
+    version='0.0.3',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jonas',
+    maintainer_email='jdech@uni-bremen.de',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
 )
-
-setup(**setup_args)
